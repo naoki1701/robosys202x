5aa52bdb607e1ff7df147920163827daba125895
naoki1701 <s20c1043nk@s.chibakoudai.jp>
2023-12-29 21:07:21 +0900

    change msg

--- ros2_ws/src/mypkg/mypkg/talker.py
+++ ros2_ws/src/mypkg/mypkg/talker.py
@@ -1,16 +1,21 @@
+# SPDX-FileCopyrightText: 2023 Naoki Kobayashi
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
-from std_msgs.srv import Query
-
-def cb(request, response):
-    if request.name == "name":
-        response.age = 22
-    else:
-        response.age = 255
-
-    return response
+from person_msgs.msg import Person
 
 rclpy.init()
 node = Node("talker")
-srv = node.create_service(Query, "query", cb)
+pub = node.create_publisher(Person, "person", 10)
+n = 0
+def cb():
+    global n
+    msg = Person()
+    msg.name = "name"
+    msg.age = 22
+    publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
 rclpy.spin(node)
